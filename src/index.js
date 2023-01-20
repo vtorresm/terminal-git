ca9f48258eac23195cac1f2d58244ea5900b0da8
Victor Torres <vatorresm88@gmail.com>
2023-01-20 17:39:38 -0500

    Importando @clack/prompts

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { intro, outro } from '@clack/prompts'
+
+intro('Asistente para la creación de commits por Victor Torres')
+
+outro('Commit creado con éxito. ¡Gracias por usar el asistente!')
