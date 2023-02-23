67977a0428c4a72ba4a01612aaf442839659e6e4
Victor Torres <vatorresm88@gmail.com>
2023-02-22 22:17:41 -0500

    Agregando commitMessage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { intro, outro } from '@clack/prompts'
+import { intro, outro, text } from '@clack/prompts'
 
 intro('Asistente para la creación de commits por Victor Torres')
+
+const commitMessage = await text({
+  message: 'Introduce el mensaje del commit:',
+  placeholder: 'Add new feature'
+})
+
+console.log(commitMessage)
 
 outro('Commit creado con éxito. ¡Gracias por usar el asistente!')
