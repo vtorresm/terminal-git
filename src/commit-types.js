247fc8afe002cd1affa2da8eb668ee0d8dd0fa05
Victor Torres <vatorresm88@gmail.com>
2023-03-10 19:22:56 -0500

    ✅ build: update icon

--- src/commit-types.js
+++ src/commit-types.js
@@ -1,16 +1,16 @@
 export const COMMIT_TYPES = {
   feat: {
-    emoji: '🆕 ',
+    emoji: '✴ ',
     description: 'Add new feature',
     release: true // que si haces un commit de este tipo, luego deberías hacer un release
   },
   fix: {
-    emoji: '🐛 ',
+    emoji: '❓',
     description: 'Submit a fix to a bug',
     release: true
   },
   perf: {
-    emoji: '⚡️ ',
+    emoji: '⚡️',
     description: 'Improve performance',
     release: true
   },
@@ -20,17 +20,17 @@ export const COMMIT_TYPES = {
     release: true
   },
   docs: {
-    emoji: '📚 ',
+    emoji: '🧩',
     description: 'Add or update documentation',
     release: false
   },
   test: {
-    emoji: '🧪 ',
+    emoji: '🧪',
     description: 'Add or update tests',
     release: false
   },
   build: {
-    emoji: '🏗️ ',
+    emoji: '✅',
     description: 'Add or update build scripts',
     release: false
   }
